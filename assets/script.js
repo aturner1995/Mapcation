b58cfac0ab092c698837d4b8529e74da377d5bf4
“anar <sehar.uzair23@gmail.com>
2023-02-27 21:38:03 -0400

    maps

--- assets/script.js
+++ assets/script.js
@@ -8,4 +8,41 @@ $(document).ready(function() {
         $(".navbar-menu").toggleClass("is-active");
   
     });
+
+    $('#map').append(map)
+    var map = L.map('map');
+    map.setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+     }).addTo(map);
+
+     navigator.geolocation.watchPosition(success, error);
+
+     var marker, circle, zoomed;
+     function success(position){
+       var lat = position.coords.latitude;
+       var lng = position.coords.longitude;
+       var accuracy= position.coords.accuracy;
+
+       if(marker){
+        map.removeLayer(marker);
+        map.removeLayer(circle);
+       }
+
+      marker= L.marker([lat, lng]).addto(map);
+      circle=L.circle([lat, lng], {radius: accuracy}).addto(map);
+      if(!zoomed){
+      zoomed= map.fitBound(circle.getBound());
+      }
+      map.setView([lat, lng]);
+   }
+//    function error(err){
+//         if(err.code===1){
+//             alert("please allow location")
+//         }else{
+//             alert("can not get current location");
+//         }
+//    }
 });
