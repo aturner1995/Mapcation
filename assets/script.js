046fd497fd290c3cc83d13e6ebacb9c8c92693df
“anar <sehar.uzair23@gmail.com>
2023-03-01 19:23:11 -0400

    working map

--- assets/script.js
+++ assets/script.js
@@ -9,15 +9,14 @@ $(document).ready(function() {
   
     });
 
-    $('#map').append(map)
     var map = L.map('map');
+    $('#map').append(map)
     map.setView([51.505, -0.09], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-     }).addTo(map);
-
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+         }).addTo(map);
      navigator.geolocation.watchPosition(success, error);
 
      var marker, circle, zoomed;
