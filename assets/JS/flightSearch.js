b390877b8e0ea93013b81795a5c7a3ee8dd4c90d
Aaron <aaturner1995@gmail.com>
2023-03-02 14:26:02 -0400

    added recent searches and the ability to add locations you would like to visit

--- assets/JS/flightSearch.js
+++ assets/JS/flightSearch.js
@@ -1,3 +1,4 @@
+// HTML ID and Class selectors with Jquery
 const returnDateEL = $('#datepicker-2');
 const departDateEl = $('#datepicker');
 const fromAirportEl = $('#from-airport');
@@ -7,26 +8,23 @@ const travelerAmountEl = $('#traveler-number');
 const searchResultsEl = $('.search-results');
 const touristResultsEl = $('.tourist-search-results');
 const restaurantResultsEL = $('.restaurant-results');
+const savedSearchesEl = $('.recent-search');
+const favLocationsEl = $('.fav-places');
 
 // Search Flights from the users provided form submission
-const searchFlightInfo = async (e) => {
-    e.preventDefault();
-    const fromCity = fromAirportEl.val().trim();
-    const toCity = toAirportEl.val().trim();
-    const departDate = departDateEl.val().trim();
-    const returnDate = returnDateEL.val().trim();
-    const travelerAmount = travelerAmountEl.val();
-
+const searchFlightInfo = async (fromCity, toCity, departDate, returnDate, travelerAmount) => {
+    // These three parameters are required for flight search. Travel suggestions only require the final destination city.
+    // Application will not call flight search API but other features will still be functional.
     if (!fromCity || !toCity || !departDate) {
         console.error("Please provide the From City, To City and the departure date");
         return;
     }
-
+    // API ID's
     const clientId = 'lLPyMvwNle3XBM8mEfVHLOflW3uOnZUY';
     const clientSecret = 'B4t2EjT9aY2jGtfA';
     const tokenUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
     let apiUrl = `https://test.api.amadeus.com/v2/shopping/flight-offers?departureDate=${departDate}&max=3&currencyCode=CAD`;
-    
+    // Changing the api URL based on the user inputs
     if (returnDate) {
         apiUrl += `&returnDate=${returnDate}`;
     }
@@ -38,6 +36,7 @@ const searchFlightInfo = async (e) => {
     }
 
     try {
+        // POST Call to API to get a token to use for the flight search API call
         const tokenResponse = await fetch(tokenUrl, {
             method: 'POST',
             headers: {
@@ -48,7 +47,7 @@ const searchFlightInfo = async (e) => {
 
         const tokenData = await tokenResponse.json();
         const token = tokenData.access_token;
-
+        // API call to get the from city IATA code for flight search
         const fromCityResponse = await fetch(`https://test.api.amadeus.com/v1/reference-data/locations/cities?keyword=${fromCity}&max=10&include=AIRPORTS`, {
             method: 'GET',
             headers: {
@@ -57,7 +56,7 @@ const searchFlightInfo = async (e) => {
         });
         const fromCityData = await fromCityResponse.json();
         const fromIataCode = fromCityData.data[0].iataCode;
-
+        // API call to get the destination IATA code for flight search
         const toCityResponse = await fetch(`https://test.api.amadeus.com/v1/reference-data/locations/cities?keyword=${toCity}&max=10&include=AIRPORTS`, {
             method: 'GET',
             headers: {
@@ -68,7 +67,7 @@ const searchFlightInfo = async (e) => {
         let toIataCode = toCityData.data[0].iataCode;
 
         apiUrl += `&originLocationCode=${fromIataCode}&destinationLocationCode=${toIataCode}`;
-
+        // Flight Search API to get final search information for display
         let flightResponse = await fetch(apiUrl, {
             method: 'GET',
             headers: {
@@ -116,9 +115,7 @@ const displayFlightResults = (data) => {
 };
 
 
-const searchTouristInfo = async (e) => {
-    e.preventDefault();
-    let city = toAirportEl.val().trim();
+const searchTouristInfo = async (city) => {
     const apiKey = 'fsq3arJhFAddvYdLjpsFcyafVbELedluIByUHga/lfOF/XM=';
     let apiUrl = `https://api.foursquare.com/v3/places/search?query=top%20picks&near=${city}&sort=POPULARITY&limit=10&exclude_all_chains=true`;
     let places = [];
@@ -183,9 +180,7 @@ const searchTouristInfo = async (e) => {
 };
 
 
-const searchRestaurantInfo = async (e) => {
-    e.preventDefault();
-    let city = toAirportEl.val().trim();
+const searchRestaurantInfo = async (city) => {
     const apiKey = 'fsq3arJhFAddvYdLjpsFcyafVbELedluIByUHga/lfOF/XM=';
     let apiUrl = `https://api.foursquare.com/v3/places/search?query=restaurants&near=${city}&sort=POPULARITY&limit=10&exclude_all_chains=true`;
     let places =[];
@@ -290,17 +285,106 @@ const displayRestaurantInfo = (places) => {
     })
 }
 
-const addLocation = () => {
-    console.log("Yo what up dawg");
+const addLocation = (e) => {
+    e.preventDefault();
+    let recentSearch = JSON.parse(localStorage.getItem('recentSearch')) || [];
+    let card = $(e.target).closest('.card');
+    let nameEl = card.find('h4');
+    console.log(nameEl.text().replace('ADD',''));
+    let imgEl = card.find('img');
+    console.log(imgEl.attr('src'));
+
+    let savedLocation = {
+        name: nameEl.text().replace('ADD',''),
+        image: imgEl.attr('src')
+    };
+
+    recentSearch[recentSearch.length - 1].savedLocations.push(savedLocation);
+    localStorage.setItem('recentSearch', JSON.stringify(recentSearch));
+    displayFavLocations();
+};
+
+const displayFavLocations = () => {
+    let recentSearch = JSON.parse(localStorage.getItem('recentSearch')) || [];
+    favLocationsEl.empty();
+    console.log(recentSearch);
+
+    recentSearch[recentSearch.length-1].savedLocations.forEach(place => {
+        console.log(place.name);
+        let locationDisplayEl = $('<button>').text(place.name).addClass('button is-primary');
+        favLocationsEl.append(locationDisplayEl);
+    })
+
+
 }
 
-// Event Listener for form submission
-searchFormEl.on('submit', e => {
-    searchFlightInfo(e);
-    searchTouristInfo(e);
-    searchRestaurantInfo(e);
-});
 
+const saveRecentSearch = (fromCity, toCity, departDate, returnDate, travelerAmount) => {
+
+    if (travelerAmount === 'Number of travelers') {
+        travelerAmount = 1;
+    }
+
+    let recentSearch = JSON.parse(localStorage.getItem('recentSearch')) || [];
+
+    const searchObj = {
+        fromCity: fromCity,
+        toCity: toCity,
+        departDate: departDate,
+        returnDate: returnDate,
+        travelerAmount: travelerAmount,
+        savedLocations: []
+    };
+
+    // Check if the search object is already in recentSearch
+    const isDuplicate = recentSearch.some(obj =>
+        obj.fromCity === fromCity &&
+        obj.toCity === toCity &&
+        obj.departDate === departDate &&
+        obj.returnDate === returnDate
+    );
+
+    if (!isDuplicate) {
+        recentSearch.push(searchObj);
+        localStorage.setItem('recentSearch', JSON.stringify(recentSearch));
+    }
+
+    displayRecentSearch(recentSearch);
+}
+
+
+const displayRecentSearch = () => {
+    let recentSearch = JSON.parse(localStorage.getItem('recentSearch')) || [];
+    savedSearchesEl.empty();
+
+    for (var i=recentSearch.length-1; i >=0; i--) {
+        let cityDisplayEl = $('<button>').text(recentSearch[i].toCity).addClass('button is-primary');
+        savedSearchesEl.append(cityDisplayEl);
+    }    
+}
+
+const init = () => {
+    displayRecentSearch();
+}
+
+const searchForm = (e) => {
+    e.preventDefault();
+    let fromCity = fromAirportEl.val().trim();
+    let toCity = toAirportEl.val().trim();
+    let departDate = departDateEl.val().trim();
+    let returnDate = returnDateEL.val().trim();
+    let travelerAmount = travelerAmountEl.val();
+
+    searchFlightInfo(fromCity, toCity, departDate, returnDate, travelerAmount);
+    searchTouristInfo(toCity);
+    searchRestaurantInfo(toCity);
+    saveRecentSearch(fromCity, toCity, departDate, returnDate, travelerAmount);
+}
+
+
+// Event Listener for form submission
+searchFormEl.on('submit', searchForm)
+// Event listener for the add location button
 $('body').on('click', 'button.add-btn', addLocation);
 
 
@@ -325,3 +409,5 @@ $(() => {
         maxDate: '+7M'
     });
 });
+
+init();
